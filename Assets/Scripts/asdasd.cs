c24f9644486ce625d47b2aeb9c5c9ea62ca52ce4
phoo09 <jwogus0213@gmail.com>
2024-08-12 17:09:36 +0900

    [feat] 완성?

---
+++ Assets/Scripts/asdasd.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asdasd : MonoBehaviour
+{
+
+    public float asd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("boll");
+        
+    }
+}
--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -2,66 +2,108 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class move : MonoBehaviour
 {
+    
 
     public float Speed = Random.Range(1f,2f);
+    public float speed;
     public Vector3 start;
     public Vector3 asd;
-    public float x;
-    public float y;
-    public float z;
-
+    public float x = Random.Range(-3f, 3f);
+    public float y = Random.Range(-3f, 3f);
+    public float z = Random.Range(-2f, 2f);
+    public Rigidbody Rigidbody;
     public GameObject obj;
+    public 
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 10.5f, 0);
-        x = Random.Range(-1f, 1f);
-        y = Random.Range(-1f, 1f);
-        start = new Vector3(x, y, 0);
-        
+
+        Rigidbody = GetComponent<Rigidbody>();
+
+        y = Random.Range(-3f, 3f);
+        x = Random.Range(-3f, 3f);
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        speed = Rigidbody.velocity.magnitude;
+        
+        if(speed >= 100f)
+        {
+            speed -= 1;
+        }
+
+        Rigidbody.velocity = new Vector3(x, y, 0);
+
+
+
+
         transform.Translate(start * Time.deltaTime);
 
         if(Input.GetKeyDown(KeyCode.Space))
         {
+            Rigidbody.velocity = Vector3.zero;
+        }
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            Rigidbody.velocity += Vector3.up * Speed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Rigidbody.velocity += Vector3.down * Speed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Rigidbody.velocity += Vector3.left * Speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Rigidbody.velocity += Vector3.right * Speed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
             transform.position = new Vector3(0, 10.5f, 0);
         }
 
-        if(Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            transform.position += Vector3.forward * Speed;
+            x = Random.Range(1, 5);
         }
+
+        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.gameObject.CompareTag("wall"))
         {
+            //transform.position = new Vector3(0, 10.5f, 0);
+
             x = -x;
-            start = new Vector3(x, y, 0);
+            Rigidbody.velocity = new Vector3(x, y, 0);
+
             Instantiate(obj);
-            Destroy(this.obj);
+
         }
         if (collision.collider.gameObject.CompareTag("top"))
         {
+            //transform.position = new Vector3(0, 10.5f, 0);
+
             y = -y;
-            start = new Vector3(x, y, 0);
+            Rigidbody.velocity = new Vector3(x, y, 0);
             Instantiate(obj);
-            Destroy(this.obj);
-        }
-        if (collision.collider.gameObject.CompareTag("boll"))
-        {
-            x = -x;
-            y = -y;
-            start = new Vector3(x, y, 0);
+
         }
     }
 }
